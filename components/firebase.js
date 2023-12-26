ac4eef4496a2218310df4fd5d9380f6aa7db6feb
AR awan <alrafay182@gmail.com>
2023-12-26 15:08:09 +0500

    exam commit

---
+++ components/firebase.js
@@ -0,0 +1,33 @@
+// import { initializeApp } from "firebase/app";
+//  const firebaseConfig = {
+//     apiKey: "AIzaSyDdUBRnejry2NT0LQVBjZXm8MdTFU2G6nM",
+//     authDomain: "semester-project-10ee1.firebaseapp.com",
+//     databaseURL: "https://semester-project-10ee1-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "semester-project-10ee1",
+//     storageBucket: "semester-project-10ee1.appspot.com",
+//     messagingSenderId: "323612033811",
+//     appId: "1:323612033811:web:2dc39b522bf85b514a50e6"
+//   };
+  
+//     const app = initializeApp(firebaseConfig);
+  
+//   export default app;
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACXt2mSi_DNp8hNmVA_SwKf1VDW7vi-IA",
+  authDomain: "my-project-125d5.firebaseapp.com",
+  databaseURL: "https://my-project-125d5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "my-project-125d5",
+  storageBucket: "my-project-125d5.appspot.com",
+  messagingSenderId: "45595691174",
+  appId: "1:45595691174:web:f6fdd4b2da5dc6fabd8c67",
+  measurementId: "G-9HNGW8PYXB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
